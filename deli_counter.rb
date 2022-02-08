d1108179d235d253b411fef600874531e03eda6d
nicholas shawn freeman <nick_freeman65@yahoo.com>
2022-02-08 15:36:03 -0600

    commit tree 37bb1968fef9605f761e582acd5518437e995adb

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+    if array == []
+        puts "The line is currently empty."
+    else 
+        string = "The line is currently:"
+        x = 0
+        while x < array.length
+            string << " #{x+1}. #{array[x]}"
+            x += 1
+        end
+        puts string 
+    end
+end
+
+def take_a_number(katz_deli, string)
+    katz_deli.push(string)
+    puts "Welcome, #{string}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(array)
+    if array == []
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift()
+        array
+    end
+end
